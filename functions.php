f6aa077ea5200b31fe46906fa5cb63bbd25480cb
Vitali Maksimov <vitamak@greeny.cs.tlu.ee>
2015-10-09 13:34:29 +0300

    asdasdasdasd

--- functions.php
+++ functions.php
@@ -11,7 +11,7 @@
 		// globals on muutuja kõigist php failidest mis on ühendatud
 		$mysqli = new mysqli($GLOBALS["servername"], $GLOBALS["server_username"], $GLOBALS["server_password"], $GLOBALS["database"]);
 		
-		$stmt = $mysqli->prepare("SELECT id, user_id, number_plate, color FROM car_plates");
+		$stmt = $mysqli->prepare("SELECT id, user_id, number_plate, color FROM car_plates WHERE deleted IS NULL");
 		$stmt->bind_result($id, $user_id, $number_plate, $color_from_db);
 		$stmt->execute();
 		
@@ -24,14 +24,17 @@
 			
 			//loon objekti
 			$car = new StdClass();
-			$car->uf = $id;
+			$car->id = $id;
+			$car->user_id = $user_id;
 			$car->number_plate = $number_plate;
+			$car->color_from_db = $color_from_db;
+			
 			
 			//lisame selle massive
 			array_push($array, $car);
-			echo "<pre>";
-			var_dump($array);
-			echo "</pre>";
+			//echo "<pre>";
+			//var_dump($array);
+			//echo "</pre>";
 			
 			//echo $color_from_db." ".$number_plate."<br>";
 		}
@@ -39,6 +42,27 @@
 		
 		$stmt->close();
 		$mysqli->close();
+		
+		return $array;
+		
+	function deleteCar($id_to_be_deleted){
+		
+		$mysqli = new mysqli($GLOBALS["servername"], $GLOBALS["server_username"], $GLOBALS["server_password"], $GLOBALS["database"]);
+		$stmt = $mysqli->prepare("UPTADE car_plates SET deleted=NOW() WHERE id=?");
+		$stmt->bind_param("i", $id_to_be_deleted);
+		
+		if($stmt->execute()){
+			
+			//sai edukalt kustutatud
+			header("Location: table.php");
+		}
+		
+		$stmt->close();
+		$mysqli->close();
+	
+	
+	}
+	
 		
 		
 	}
--- table.php
+++ table.php
@@ -3,11 +3,56 @@
 
 
 
-//table php
+	//table php
+	require_once("functions.php");
 
-
-
-
+	//kas kasutaja tahab kustuda
+	//kas aadressireal on ?delete=???!???!!?
+	
+	if(isset($_GET["delete"])){
+		
+		//saadan kaasa id, mida kustuda
+		deleteCar($_GET["delete"]);
+		
+	}
+	
+	
+	$car_list = getCarData();
+	//var_dump($car_list);
 
 
 ?>
+<table border=1 >
+	<tr>
+
+		<th>id</th>
+		<th>user id</th>
+		<th>number plate</th>
+		<th>color</th>
+		<th>X</th>
+
+	</tr>
+
+	<?php
+	
+	
+	
+		for($i = 0; $i < count($car_list); $i++){
+			// $i = $i +1; sama mis $i += 1; sama mis $i++;
+			echo "<tr>";
+			
+			echo "<td>".$car_list[$i]->id."</td>";
+			echo "<td>".$car_list[$i]->user_id."</td>";
+			echo "<td>".$car_list[$i]->number_plate."</td>";
+			echo "<td>".$car_list[$i]->color_from_db."</td>";
+			echo "<td><a href='?delete=".$car_list[$i]->id."'>X</a></td>";
+			
+			echo "</tr>";
+		}
+			
+			
+		
+	?>	
+		
+		
+</table>
