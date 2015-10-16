1dbaa6a03fcb0e5a73317ee498435c3b56605170
Vitali Maksimov <vitamak@greeny.cs.tlu.ee>
2015-10-16 10:42:21 +0300

    added nuppud edit

--- functions.php
+++ functions.php
@@ -4,69 +4,69 @@
 	$database = "if15_vitamak";
 
 	
-	//loome uue funktsioni, et küsida abist andmed
+		//loome uue funktsiooni, et küsida ab'ist andmeid
 	function getCarData(){
 		
-		// lisame kasutaja ab'i
-		// globals on muutuja kõigist php failidest mis on ühendatud
 		$mysqli = new mysqli($GLOBALS["servername"], $GLOBALS["server_username"], $GLOBALS["server_password"], $GLOBALS["database"]);
 		
 		$stmt = $mysqli->prepare("SELECT id, user_id, number_plate, color FROM car_plates WHERE deleted IS NULL");
 		$stmt->bind_result($id, $user_id, $number_plate, $color_from_db);
 		$stmt->execute();
 		
+		// tühi massiiv kus hoiame objekte (1 rida andmeid)
 		$array = array();
 		
-		//tee tsüklit nii mitu korda, kui saad
-		//abist ühe rea andmed
-		
+		// tee tsüklit nii mitu korda, kui saad 
+		// ab'ist ühe rea andmeid
 		while($stmt->fetch()){
 			
-			//loon objekti
+			// loon objekti iga while tsükli kord
 			$car = new StdClass();
 			$car->id = $id;
-			$car->user_id = $user_id;
 			$car->number_plate = $number_plate;
-			$car->color_from_db = $color_from_db;
+			$car->user_id = $user_id;
+			$car->color = $color_from_db;
 			
-			
-			//lisame selle massive
+			// lisame selle massiivi
 			array_push($array, $car);
 			//echo "<pre>";
 			//var_dump($array);
 			//echo "</pre>";
 			
-			//echo $color_from_db." ".$number_plate."<br>";
 		}
-		
 		
 		$stmt->close();
 		$mysqli->close();
 		
 		return $array;
 		
+		
+	}
+	
+	
 	function deleteCar($id_to_be_deleted){
 		
 		$mysqli = new mysqli($GLOBALS["servername"], $GLOBALS["server_username"], $GLOBALS["server_password"], $GLOBALS["database"]);
-		$stmt = $mysqli->prepare("UPTADE car_plates SET deleted=NOW() WHERE id=?");
+		
+		$stmt = $mysqli->prepare("UPDATE car_plates SET deleted=NOW() WHERE id=?");
 		$stmt->bind_param("i", $id_to_be_deleted);
 		
 		if($stmt->execute()){
-			
-			//sai edukalt kustutatud
+			// sai edukalt kustutatud
 			header("Location: table.php");
+			
 		}
 		
 		$stmt->close();
 		$mysqli->close();
-	
-	
-	}
-	
-		
 		
 	}
 	
-	getCarData();
+	
+	
+	//$name = "Romil";
+	//echo "Tere ".$name;
+	
+	//var_dump("asdasdasdasd");
 	
 ?>
--- table.php
+++ table.php
@@ -36,20 +36,42 @@
 	<?php
 	
 	
-	
+		// iga massiivis olema elemendi kohta
+		// count($car_list) - massiivi pikkus
 		for($i = 0; $i < count($car_list); $i++){
 			// $i = $i +1; sama mis $i += 1; sama mis $i++;
-			echo "<tr>";
 			
-			echo "<td>".$car_list[$i]->id."</td>";
-			echo "<td>".$car_list[$i]->user_id."</td>";
-			echo "<td>".$car_list[$i]->number_plate."</td>";
-			echo "<td>".$car_list[$i]->color_from_db."</td>";
-			echo "<td><a href='?delete=".$car_list[$i]->id."'>X</a></td>";
+			//kui on see rida mida kasutaja tahab muuta siis kuvan input väljad
+			if(isset($_GET["edit"]) && $car_list[$i]->id == $_GET["edit"]){
+				// kasutajale muutmiseks
+				echo "<tr>";
+					echo "<form action='table.php' method='post'>";
+						echo "<td>".$car_list[$i]->id."</td>";
+						echo "<td>".$car_list[$i]->user_id."</td>";
+						echo "<td><input name='number_plate' value='".echo "<td>".$car_list[$i]->number_plate."</td>"."'></td>";
+						echo "<td><input name='color' value='".echo "<td>".$car_list[$i]->color."</td>"."'></td>";
+						echo "<td><input type='submit' name='update'></td>";
+						echo "<td><a href='table.php'>cancel</a></td>";
+					echo "</form>";
+				echo "</tr>";
+				
+			}else{
+				// tavaline rida
+				echo "<tr>";
 			
-			echo "</tr>";
+				echo "<td>".$car_list[$i]->id."</td>";
+				echo "<td>".$car_list[$i]->user_id."</td>";
+				echo "<td>".$car_list[$i]->number_plate."</td>";
+				echo "<td>".$car_list[$i]->color."</td>";
+				echo "<td><a href='?delete=".$car_list[$i]->id."'>X</a></td>";
+				echo "<td><a href='?edit=".$car_list[$i]->id."'>edit</a></td>";
+			
+				echo "</tr>";
+			}
+			
+			
 		}
-			
+	
 			
 		
 	?>	
